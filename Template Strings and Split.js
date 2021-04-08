1c0ca4fc7bc6506213206c1bbce9f82d4fa7cf63
Deepak-worthy <82026071+Deepak-worthy@users.noreply.github.com>
2021-04-08 20:07:50 +0530

    Program to demonstrate template string and Split

---
+++ Template Strings and Split.js
@@ -0,0 +1,7 @@
+let name="Deepak Dalal";
+let age=23
+let address="Vasant Vihar, Rohtak, Haryana"
+let personDetails=`My name is ${name} , age ${age}, address is ${address}`;
+console.log(personDetails);
+
+console.log(personDetails.split(", "))
