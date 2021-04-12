ab3633d4bcdcdb89637739b2d426e721968adcdb
Deepak Dalal <dee24da@gmail.com>
2021-04-13 02:25:55 +0530

    Added simple code for GetElemetbyClassname

--- DOM manipulation/script.js
+++ DOM manipulation/script.js
@@ -1,30 +1,45 @@
-//EXAMINE THE DOCUMENT OBJECT 
-console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-//document.title=123;
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-// document.all[10].textContent = 'Hello';
-console.log(document.forms[0]);
-console.log(document.links);
-console.log(document.images);
+// //EXAMINE THE DOCUMENT OBJECT 
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
 
-//GETELEMENTBYID
-console.log(document.getElementById("header-title"));
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = "Goodbye';
-// console.log(headerTitle.innerText);
-// headerTitle.innerHTML = '<h3>Hello</h3>';
-header.style.borderBottom='solid 3px #000';
+// //GETELEMENTBYID
+// console.log(document.getElementById("header-title"));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// // console.log(headerTitle);
+// // headerTitle.textContent = 'Hello';
+// // headerTitle.innerText = "Goodbye';
+// // console.log(headerTitle.innerText);
+// // headerTitle.innerHTML = '<h3>Hello</h3>';
+// header.style.borderBottom='solid 3px #000';
 
-let addItem=document.getElementById("additems");
-addItem.style.color="green";
-addItem.style.fontWeight="bold";
+// let addItem=document.getElementById("additems");
+// addItem.style.color="green";
+// addItem.style.fontWeight="bold";
+
+// GETELEMENTSBYCLASSNAME //
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+//items[1].textContent = 'Hello 2';
+//items[1].style.fontWeight = "bold";
+items[2].style.backgroundColor = 'green';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < items.length; i++){
+items[i].style.fontWeight = 'bold';
+}
