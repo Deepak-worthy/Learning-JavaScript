e80940ad7a5d57667db0ab035d6afb14949d855e
Deepak Dalal <dee24da@gmail.com>
2021-04-13 14:38:09 +0530

    Added code for querySelector and querySelectorAll

--- DOM manipulation/script.js
+++ DOM manipulation/script.js
@@ -1,10 +1,15 @@
-let li = document.getElementsByClassName('list-group-item');
+// // QUERYSELECTOR //
+let  secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+let  thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = 'hidden';
+
+// QUERYSELECTORALL 
+let li = document.querySelectorAll("li");
 console.log(li);
+li[1].style.color="green";
 
-//gives error
-//li[4].style.backgroundColor = 'yellow';
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i = 0; i < odd.length; i++)
+    odd[i].style.backgroundColor = 'green';
 
-// GETELEMENTSBYTAGNAME //
-let liByTag = document.getElementsByTagName('li');
-
-liByTag[4].style.backgroundColor = 'yellow';
