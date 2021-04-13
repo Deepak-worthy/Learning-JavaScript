2cf5279c99791c929d66dce3ec8db9377c867bde
Deepak Dalal <dee24da@gmail.com>
2021-04-13 13:51:53 +0530

    Added code for getElementByTagName

--- DOM manipulation/script.js
+++ DOM manipulation/script.js
@@ -1,45 +1,10 @@
-// //EXAMINE THE DOCUMENT OBJECT 
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// //document.title=123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[10]);
-// // document.all[10].textContent = 'Hello';
-// console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
+let li = document.getElementsByClassName('list-group-item');
+console.log(li);
 
-// //GETELEMENTBYID
-// console.log(document.getElementById("header-title"));
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-// // console.log(headerTitle);
-// // headerTitle.textContent = 'Hello';
-// // headerTitle.innerText = "Goodbye';
-// // console.log(headerTitle.innerText);
-// // headerTitle.innerHTML = '<h3>Hello</h3>';
-// header.style.borderBottom='solid 3px #000';
+//gives error
+//li[4].style.backgroundColor = 'yellow';
 
-// let addItem=document.getElementById("additems");
-// addItem.style.color="green";
-// addItem.style.fontWeight="bold";
+// GETELEMENTSBYTAGNAME //
+let liByTag = document.getElementsByTagName('li');
 
-// GETELEMENTSBYCLASSNAME //
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-//items[1].textContent = 'Hello 2';
-//items[1].style.fontWeight = "bold";
-items[2].style.backgroundColor = 'green';
-
-// Gives error
-//items.style.backgroundColor = '#f4f4f4';
-
-for(var i = 0; i < items.length; i++){
-items[i].style.fontWeight = 'bold';
-}
+liByTag[4].style.backgroundColor = 'yellow';
