5b1ffef055d9365cb1d1d82c92d4a09d3345873c
Deepak Dalal <dee24da@gmail.com>
2021-04-13 16:09:14 +0530

    Added code for Creating Nodes And Modifying Dom

--- DOM manipulation/script.js
+++ DOM manipulation/script.js
@@ -1,15 +1,59 @@
-// // QUERYSELECTOR //
-let  secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
-let  thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.visibility = 'hidden';
+// TRAVERSING THE DOM //
+var itemlist =document.querySelector('#items');
+// parentNode
+console.log(itemlist.parentNode);
+itemlist.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemlist.parentNode.parentNode.parentNode);
+// parentElement
+console.log(itemlist.parentElement);
+itemlist.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemlist.parentElement.parentElement.parentElement);
+// childNodes
+console.log(itemlist.childNodes);
+console.log(itemlist.children);
+console.log(itemlist.children[1]);
+itemlist.children[1].style.backgroundColor = 'yellow';
+// FirstChild
+console.log(itemlist.firstChild);
+// firstElementChild
+console.log(itemlist.firstElementChild);
+itemlist.firstElementChild.textContent = 'Hello 1';
 
-// QUERYSELECTORALL 
-let li = document.querySelectorAll("li");
-console.log(li);
-li[1].style.color="green";
+// lastChild
+console.log(itemlist.lastChild);
+// lastElementChild
+console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.textcontent ='Hello 4';
+// nextSibling
+console.log(itemlist.nextsibling);
+//nextElementsibling
+console.log(itemlist.nextElementSibling);
+// previoussibling
+console.log(itemlist.previousSibling);
+// previousElementsibling
+console.log(itemlist.previousElementsibling);
+//itemlist.previousElementsibling.style.color = 'green';
+// createElement
+// Create a div
+var newDiv = document.createElement('div');
+// Add class
+newDiv.className= 'hello';
+// Add id
+newDiv.id = 'hello l';
+console.log(newDiv);
 
-let odd = document.querySelectorAll('li:nth-child(odd)');
-for(let i = 0; i < odd.length; i++)
-    odd[i].style.backgroundColor = 'green';
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+// Create text node
+var newDivText = document.createTextNode('Hello world');
+// Add text to div
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);
 
+var container2 = document.querySelector('#items');
+var li = document.querySelector('.list-group-item');
+container2.insertBefore(newDiv, li);
