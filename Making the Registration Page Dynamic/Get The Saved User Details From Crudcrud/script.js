e0e086dfc6abab15f0c7b3bd62e008fcccb0b9da
Deepak-worthy <82026071+Deepak-worthy@users.noreply.github.com>
2021-04-28 16:12:58 +0530

    Added files for get request to crudcrud assignment

---
+++ Making the Registration Page Dynamic/Get The Saved User Details From Crudcrud/script.js
@@ -0,0 +1,37 @@
+const formElement=document.querySelector('#form-element');
+formElement.addEventListener('submit',onSubmit);
+
+// GET REQUEST
+axios({
+    method:'get',
+    url:'https://crudcrud.com/api/d301b7b77a5d42119e8d8558b0e4db13/registeruser'
+    })
+    .then(res => showOutput(res))
+    .catch(err => console.error(err)); 
+function onSubmit(e)
+{
+    e.preventDefault();
+    let nm=document.querySelector('#name');
+    let em=document.querySelector('#email');
+    let ph=document.querySelector('#phoneNo')    
+    
+ 
+    // POST REQUEST
+    axios({
+        method:'post',
+        url:'https://crudcrud.com/api/d301b7b77a5d42119e8d8558b0e4db13/registeruser', 
+        data:{
+            name: nm.value,
+            email:em.value,
+            phone:ph.value
+        }
+        })
+        .then(res => showOutput(res))
+        .catch(err => console.error(err));
+       
+}
+
+function showOutput(res) {
+    document.querySelector('#studentDetails').innerHTML =
+    `<pre>${JSON.stringify(res.data, null, 2)}</pre>`;  
+}
